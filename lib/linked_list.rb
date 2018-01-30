0477481ce6c8ac7e07a9542a04f3356d04657a1b
agpiermarini <piermarini.andrew@gmail.com>
2018-01-30 16:54:15 -0700

    progress on Iteration 2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,22 +1,37 @@
-require "./lib/node"
 
+require "./lib/node"
 class LinkedList
 
-attr_reader :head,
-            :count
+attr_accessor   :head,
+                :tail,
+                :count
 
     def initialize
         @head = nil
+        @tail = nil
         @count = 0
     end
 
     def append(surname)
         @count += 1
-        @head = Node.new(surname)
+
+        new_node = Node.new(surname)
+
+        if @head == nil
+            @head = new_node
+            @tail = @head
+        elsif head.next_node == nil
+            head.next_node = new_node
+            @tail = new_node
+        else
+            tail.next_node = new_node
+            @tail = new_node
+            #how do we scan through nodes?
+        end
     end
 
     def to_string
-        p "The #{@head.surname} family"
+        "The #{head.surname} family"
     end
 
 end
