494aa58ec1c6611ca19bbcbd3120509b67c75d4d
agpiermarini <piermarini.andrew@gmail.com>
2018-01-30 16:54:41 -0700

    progress on Iteration 2

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -15,18 +15,23 @@ class LinkedListTest < Minitest::Test
         assert_nil list.head
     end
 
-    def test_node_can_be_added
+    # def test_it_has_tail_nil_by_default
+    #     list = LinkedList.new
+    #     assert_nil list.tail
+    # end
+
+    def test_can_append_node
         list = LinkedList.new
         assert_instance_of Node, list.append("West")
     end
 
-    def test_first_node_updates_head
+    def test_first_append_updates_head
         list = LinkedList.new
         list.append("West")
         assert_equal "West", list.head.surname
     end
 
-    def test_first_node_points_to_nil
+    def test_appended_node_points_to_nil
         list = LinkedList.new
         list.append("West")
         assert_nil list.head.next_node
@@ -42,6 +47,29 @@ class LinkedListTest < Minitest::Test
         list = LinkedList.new
         list.append("West")
         assert_equal "The West family", list.to_string
+    end
+
+##Iteration 2
+    def test_can_append_multiple_nodes
+        list = LinkedList.new
+
+        assert_instance_of Node, list.append("Rhodes") #do i need assert_instance_of
+        assert_equal "Rhodes", list.head.surname
+        assert_nil list.head.next_node
+        assert_equal "Rhodes", list.tail.surname
+
+        assert_instance_of Node, list.append("Hardy")
+        assert_equal "Rhodes", list.head.surname
+        assert_equal "Hardy", list.head.next_node.surname
+        assert_equal "Hardy", list.tail.surname
+        list.head.next_node.next_node
+
+        assert_instance_of Node, list.append("Smith")
+        # binding.pry
+        assert_equal "Rhodes", list.head.surname
+        assert_equal "Hardy", list.head.next_node.surname
+        assert_equal "Smith", list.head.next_node.next_node.surname
+        assert_equal "Smith", list.tail.surname
     end
 
 end
