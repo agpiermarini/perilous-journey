a283fd348184e2e94a4c3c1414dd1c9d52390e84
agpiermarini <piermarini.andrew@gmail.com>
2018-01-31 00:54:33 -0700

    completed iteration 3

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,26 +12,138 @@ attr_accessor   :head,
         @count = 0
     end
 
-    def append(surname)
-        @count += 1
+    def count
+        head.nil? ? @count = 0 : (                  #is this acceptable, discouraged? because of alignment/style or use of ternary, etc?
 
+        @count = 1
+        current_node = head
+        until current_node.next_node.nil?
+            @count += 1
+            current_node = current_node.next_node
+        end
+
+        @count
+        )
+    end
+
+    def append(surname)
         new_node = Node.new(surname)
 
-        if @head == nil
+        @head.nil? ? (@head = new_node; @tail = @head) :                #what about this?
+                     (@tail.next_node = new_node; @tail = new_node)
+    end
+
+    def prepend(surname)
+        new_node = Node.new(surname)
+
+        if @head.nil?
             @head = new_node
             @tail = @head
-        elsif head.next_node == nil
-            head.next_node = new_node
-            @tail = new_node
         else
-            tail.next_node = new_node
-            @tail = new_node
-            #how do we scan through nodes?
+            previous_head = @head
+            @head = new_node
+            @head.next_node = previous_head
+        end
+    end
+
+    def insert(position, surname)
+        if @head.nil? || position <= 0
+            prepend(surname)
+        elsif position >= count                 #why doesnt this pass when @count?
+            append(surname)
+        else
+            offset = position - 1
+            preceeding_node = head
+            proceeding_node = head.next_node
+
+            offset.times do
+                preceeding_node = preceeding_node.next_node
+                proceeding_node = proceeding_node.next_node
+            end
+            preceeding_node.next_node = Node.new(surname)
+            preceeding_node.next_node.next_node = proceeding_node
         end
     end
 
     def to_string
-        "The #{head.surname} family"
+        head.nil? ? (family_names = "No families have been added to the wagon train"):
+                    (family_names = "The #{head.surname} family"; current_node = head.next_node)
+
+        until current_node.nil?
+            family_names += ", followed by the #{current_node.surname} family"
+            current_node = current_node.next_node
+        end
+
+        return family_names
     end
 
 end
+
+
+
+
+##DELETE BEFORE FINAL COMMIT
+# def count
+#
+#     if head.nil?
+#         @count = 0
+#     else
+#         current_node = head
+#         @count = 1
+#         until current_node.next_node.nil?
+#             @count += 1
+#             current_node = current_node.next_node
+#         end
+#         @count
+#     end
+# end
+
+# def count
+#     head.nil? ? @count = 0 : @count = 1
+#
+#     current_node = head
+#     until current_node.next_node.nil?
+#         @count += 1
+#         current_node = current_node.next_node
+#     end
+#
+#     @count
+# end
+
+# def append
+#   if @head == nil                      #clean up redundancies---only two conditions if/else, either head is nil or not
+#       @head = new_node
+#       @tail = @head
+#   elsif head.next_node == nil
+#       head.next_node = new_node
+#       @tail = new_node
+#   else
+#       tail.next_node = new_node
+#       @tail = new_node
+#   end
+# end
+
+#def append
+#     if @head == nil                     #clean up redundancies---only two conditions if/else, either head is nil or not
+#         @head = new_node
+#         @tail = @head
+#     else
+#         @tail.next_node = new_node
+#         @tail = new_node
+#     end
+# end
+
+# def to_string
+#
+#     family_names = "The #{head.surname} family"
+#
+#     current_node = head.next_node
+#
+#     until current_node.nil?
+#         family_names += ", followed by the #{current_node.surname} family"
+#         current_node = current_node.next_node
+#     end
+#
+#     return family_names
+#
+# end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -15,10 +15,10 @@ class LinkedListTest < Minitest::Test
         assert_nil list.head
     end
 
-    # def test_it_has_tail_nil_by_default
-    #     list = LinkedList.new
-    #     assert_nil list.tail
-    # end
+    def test_it_has_tail_nil_by_default
+        list = LinkedList.new
+        assert_nil list.tail
+    end
 
     def test_can_append_node
         list = LinkedList.new
@@ -39,37 +39,127 @@ class LinkedListTest < Minitest::Test
 
     def test_nodes_can_be_counted
         list = LinkedList.new
+        assert_equal 0, list.count
         list.append("West")
         assert_equal 1, list.count
+        list.append("Adams")
+        assert_equal 2, list.count
+        list.append("Hardy")
+        assert_equal 3, list.count
+        list.append("Smith")
+        assert_equal 4, list.count
+        assert_equal 4, list.count
     end
 
-    def test_generates_string_with_family_names
-        list = LinkedList.new
-        list.append("West")
-        assert_equal "The West family", list.to_string
-    end
+    # def test_generates_string_with_family_names
+    #     list = LinkedList.new
+    #     list.append("West")
+    #     assert_equal "The West family", list.to_string
+    # end
 
 ##Iteration 2
     def test_can_append_multiple_nodes
         list = LinkedList.new
 
-        assert_instance_of Node, list.append("Rhodes") #do i need assert_instance_of
-        assert_equal "Rhodes", list.head.surname
+        assert_instance_of Node, list.append("West") #do i need assert_instance_of
+        assert_equal "West", list.head.surname
         assert_nil list.head.next_node
-        assert_equal "Rhodes", list.tail.surname
 
-        assert_instance_of Node, list.append("Hardy")
-        assert_equal "Rhodes", list.head.surname
-        assert_equal "Hardy", list.head.next_node.surname
-        assert_equal "Hardy", list.tail.surname
-        list.head.next_node.next_node
+        assert_instance_of Node, list.append("Adams")
+        assert_equal "West", list.head.surname
+        assert_equal "Adams", list.head.next_node.surname
+        assert_equal "Adams", list.tail.surname
 
         assert_instance_of Node, list.append("Smith")
-        # binding.pry
-        assert_equal "Rhodes", list.head.surname
-        assert_equal "Hardy", list.head.next_node.surname
+        assert_equal "West", list.head.surname
+        assert_equal "Adams", list.head.next_node.surname
         assert_equal "Smith", list.head.next_node.next_node.surname
         assert_equal "Smith", list.tail.surname
+
+        assert_instance_of Node, list.append("Hardy")
+        assert_equal "West", list.head.surname
+        assert_equal "Adams", list.head.next_node.surname
+        assert_equal "Smith", list.head.next_node.next_node.surname
+        assert_equal "Hardy", list.head.next_node.next_node.next_node.surname
+        assert_equal "Hardy", list.tail.surname
+    end
+
+    def test_generates_string_with_multiple_family_names
+        list = LinkedList.new
+        list.append("West")
+        list.append("Adams")
+        list.append("Smith")
+        list.append("Hardy")
+        assert_equal ("The West family, followed by the Adams family, followed by the Smith family, followed by the Hardy family"), list.to_string
+    end
+
+#Iteration 3
+    def test_can_prepend_nodes
+        list = LinkedList.new
+        list.append("West")
+        list.append("Adams")
+
+        list.prepend("Smith")
+        assert_equal "Smith", list.head.surname
+        assert_equal "West", list.head.next_node.surname
+        assert_equal "Adams", list.head.next_node.next_node.surname
+        assert_equal "Adams", list.tail.surname
+        assert_equal 3, list.count
+    end
+
+    def test_can_prepend_nodes_when_list_empty
+        list = LinkedList.new
+
+        list.prepend("West")
+        assert_equal "West", list.head.surname
+        assert_equal "West", list.tail.surname
+        assert_equal 1, list.count
+    end
+
+    def test_can_insert_nodes_in_index
+        list = LinkedList.new
+        list.append("West")
+        list.append("Adams")
+        list.append("Smith")
+
+        list.insert(1, "Hardy")
+        assert_equal "West", list.head.surname
+        assert_equal "Hardy", list.head.next_node.surname
+        assert_equal "Adams", list.head.next_node.next_node.surname
+        assert_equal "Smith", list.head.next_node.next_node.next_node.surname
+        assert_equal "Smith", list.tail.surname
+        assert_equal 4, list.count
+    end
+
+    def test_can_insert_nodes_below_index
+        list = LinkedList.new
+        list.append("West")
+        list.append("Adams")
+        list.append("Smith")
+
+        list.insert(-2, "Rhodes")
+        assert_equal "Rhodes", list.head.surname
+        assert_equal "West", list.head.next_node.surname
+        assert_equal "Adams", list.head.next_node.next_node.surname
+        assert_equal "Smith", list.head.next_node.next_node.next_node.surname
+        assert_equal "Smith", list.tail.surname
+        assert_equal 4, list.count
+    end
+
+    def test_can_insert_nodes_above_index
+        #skip
+        list = LinkedList.new
+        list.append("West")
+        list.append("Adams")
+        list.append("Smith")
+
+        list.insert(5, "Davies")
+        assert_equal "West", list.head.surname
+        assert_equal "Adams", list.head.next_node.surname
+        assert_equal "Smith", list.head.next_node.next_node.surname
+        assert_equal "Davies", list.head.next_node.next_node.next_node.surname
+        assert_equal "Davies", list.tail.surname
+        assert_equal 4, list.count
     end
 
 end
